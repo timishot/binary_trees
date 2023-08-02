af59c05ad77eb9408f457263b19acb7b1bda232d
timishot <timidotcom58@gmail.com>
2023-08-02 18:35:43 +0100

    update my code

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,8 +4,7 @@
  * binary_tree_node - creating a binary tree node
  * @parent: parent is a pointer to the parent node of the node to create
  * @value: value is the value to put in the new node
- * Return: If an error occurs - NULL.
- * 	otherwise - a pointer to the new node.
+ * Return: If an error occurs - NULL.otherwise - a pointer to the new node.
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
