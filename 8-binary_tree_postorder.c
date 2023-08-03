68620cc485668eb4a7fd5e7d955da64fb6924302
timishot <timidotcom58@gmail.com>
2023-08-03 18:18:46 +0100

    made some update

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -13,5 +13,5 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 	binary_tree_postorder(tree->left, func);
 	binary_tree_postorder(tree->right, func);
-    func(tree->n);
+	func(tree->n);
 }
