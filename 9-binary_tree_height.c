6fa8efd5bd02c7ad8076092b153b2e271c1fa829
timishot <timidotcom58@gmail.com>
2023-08-04 16:29:26 +0100

    more on tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,11 +10,15 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		size_t l = 0, r = 0;
+		size_t left_height = 0, right_height = 0;
 
-		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		return ((l > r) ? l : r);
+		if (tree->left)
+			left_height = 1 + binary_tree_height(tree->left);
+
+		if (tree->right)
+			right_height = 1 + binary_tree_height(tree->right);
+
+		return (left_height > right_height ? left_height : right_height);
 	}
 
 	return (0);
