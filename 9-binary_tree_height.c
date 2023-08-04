ef9f0c0953c25ecb5b35701eec2c770aa96ae69e
timishot <timidotcom58@gmail.com>
2023-08-04 02:49:59 +0100

    update on the ode

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,17 +10,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	else
-	{
-		/* compute the depth of each subtree */
-		size_t left_height = binary_tree_height(tree->left);
-		size_t right_height = binary_tree_height(tree->right);
+	/* compute the depth of each subtree */
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
 
-		return (1 + (left_height > right_height ? left_height : right_height));
-		/*if (left_height > right_height)
-			return (left_height + 1);
-		else
-			return (right_height + 1);
-			*/
-	}
+	if (left_height > right_height)
+		return (left_height + 1);
+	return (right_height + 1);
 }
