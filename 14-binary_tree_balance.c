2e9e07d3a3c3e040016bbc7d6ee02bfe8760355a
timishot <timidotcom58@gmail.com>
2023-08-08 00:49:48 +0100

    more on tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_balance -  function tht counts the nodes with at least 1
+ *  in binary tree
+ *  @tree: is   ointet to the root node
+ *  Return: return 0 id tree is null or the number of nodes
+ **/
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
@@ -7,12 +13,11 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree == 0)
 		return (0);
-	
+
 	if (tree->left)
 		left_node = 1 + binary_tree_balance(tree->left);
-
 	if (tree->right)
 		right_node = 1 + binary_tree_balance(tree->right);
 
-	return (left_node - right_node);		
+	return (left_node - right_node);
 }
